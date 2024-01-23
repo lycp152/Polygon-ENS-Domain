1c13b0d621556e7ce2da55c0c783ca4b04be46e1
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-23 13:30:11 +0900

    feat: S4-L2まで

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -20,6 +20,9 @@ const App = () => {
   const [domain, setDomain] = useState("");
   const [record, setRecord] = useState("");
   const [network, setNetwork] = useState("");
+  const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [mints, setMints] = useState([]);
 
   const connectWallet = async () => {
     try {
@@ -117,19 +120,21 @@ const App = () => {
   };
 
   const mintDomain = async () => {
-    // ドメインがnullのときrunしません。
+    // domain の存在確認です。
     if (!domain) {
       return;
     }
-    // ドメインが3文字に満たない、短すぎる場合にアラートを出します。
+    // ドメインが短すぎる場合アラートを出します。
     if (domain.length < 3) {
       alert("Domain must be at least 3 characters long");
       return;
     }
-    // ドメインの文字数に応じて価格を計算します。
-    // 3 chars = 0.05 MATIC, 4 chars = 0.03 MATIC, 5 or more = 0.01 MATIC
+
+    // ドメインの文字数で価格を計算します。
+    // 3文字 = 0.005 MATIC, 4文字 = 0.003 MATIC, 5文字以上 = 0.001 MATIC
+    // ご自分で設定を変えても構いませんが、現在ウォレットには少量しかないはずです。。。
     const price =
-      domain.length === 3 ? "0.05" : domain.length === 4 ? "0.03" : "0.01";
+      domain.length === 3 ? "0.005" : domain.length === 4 ? "0.003" : "0.001";
     console.log("Minting domain", domain, "with price", price);
     try {
       const { ethereum } = window;
@@ -146,22 +151,27 @@ const App = () => {
         let tx = await contract.register(domain, {
           value: ethers.utils.parseEther(price),
         });
-        // ミントされるまでトランザクションを待ちます。
+        // トランザクションを待ちます
         const receipt = await tx.wait();
 
-        // トランザクションが問題なく実行されたか確認します。
+        // トランザクションの成功の確認です。
         if (receipt.status === 1) {
           console.log(
             "Domain minted! https://mumbai.polygonscan.com/tx/" + tx.hash
           );
 
-          // domain,recordをセットします。
+          // domain の record をセットします。
           tx = await contract.setRecord(domain, record);
           await tx.wait();
 
           console.log(
             "Record set! https://mumbai.polygonscan.com/tx/" + tx.hash
           );
+
+          // fetchMints関数実行後2秒待ちます。
+          setTimeout(() => {
+            fetchMints();
+          }, 2000);
 
           setRecord("");
           setDomain("");
@@ -172,6 +182,82 @@ const App = () => {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const fetchMints = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        // もう理解できていますね。
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractAbi.abi,
+          signer
+        );
+
+        // すべてのドメインを取得します。
+        const names = await contract.getAllNames();
+
+        // ネームごとにレコードを取得します。マッピングの対応を理解しましょう。
+        const mintRecords = await Promise.all(
+          names.map(async (name) => {
+            const mintRecord = await contract.records(name);
+            const owner = await contract.domains(name);
+            return {
+              id: names.indexOf(name),
+              name: name,
+              record: mintRecord,
+              owner: owner,
+            };
+          })
+        );
+
+        console.log("MINTS FETCHED ", mintRecords);
+        setMints(mintRecords);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // currentAccount, network が変わるたびに実行されます。
+  useEffect(() => {
+    if (network === "Polygon Mumbai Testnet") {
+      fetchMints();
+    }
+  }, [currentAccount, network]);
+
+  const updateDomain = async () => {
+    if (!record || !domain) {
+      return;
+    }
+    setLoading(true);
+    console.log("Updating domain", domain, "with record", record);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractAbi.abi,
+          signer
+        );
+
+        let tx = await contract.setRecord(domain, record);
+        await tx.wait();
+        console.log("Record set https://mumbai.polygonscan.com/tx/" + tx.hash);
+
+        fetchMints();
+        setRecord("");
+        setDomain("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
   };
 
   // レンダリング関数です。
@@ -190,12 +276,10 @@ const App = () => {
 
   // ドメインネームとデータの入力フォームです。
   const renderInputForm = () => {
-    // Polygon Mumbai Testnet上にいない場合、switchボタンをレンダリングします。
     if (network !== "Polygon Mumbai Testnet") {
       return (
         <div className="connect-wallet-container">
-          <h2>Please switch to Polygon Mumbai Testnet</h2>
-          {/* 今ボタンで switchNetwork 関数を呼び出します。 */}
+          <p>Please connect to Polygon Mumbai Testnet</p>
           <button className="cta-button mint-button" onClick={switchNetwork}>
             Click here to switch
           </button>
@@ -221,15 +305,93 @@ const App = () => {
           placeholder="whats ur nyanko power?"
           onChange={(e) => setRecord(e.target.value)}
         />
-
-        <div className="button-container">
-          {/* ボタンクリックで mintDomain関数 を呼び出します。 */}
-          <button className="cta-button mint-button" onClick={mintDomain}>
+        {/* editing 変数が true の場合、"Set record" と "Cancel" ボタンを表示します。 */}
+        {editing ? (
+          <div className="button-container">
+            {/* updateDomain関数を呼び出します。 */}
+            <button
+              className="cta-button mint-button"
+              disabled={loading}
+              onClick={updateDomain}
+            >
+              Set record
+            </button>
+            {/* editing を false にしてEditモードから抜けます。*/}
+            <button
+              className="cta-button mint-button"
+              onClick={() => {
+                setEditing(false);
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        ) : (
+          // editing 変数が true でない場合、Mint ボタンが代わりに表示されます。
+          <button
+            className="cta-button mint-button"
+            disabled={loading}
+            onClick={mintDomain}
+          >
             Mint
           </button>
-        </div>
+        )}
       </div>
     );
+  };
+
+  const renderMints = () => {
+    if (currentAccount && mints.length > 0) {
+      return (
+        <div className="mint-container">
+          <p className="subtitle"> Recently minted domains!</p>
+          <div className="mint-list">
+            {mints.map((mint, index) => {
+              return (
+                <div className="mint-item" key={index}>
+                  <div className="mint-row">
+                    <a
+                      className="link"
+                      href={`https://testnets.opensea.io/assets/mumbai/${CONTRACT_ADDRESS}/${mint.id}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <p className="underlined">
+                        {" "}
+                        {mint.name}
+                        {tld}{" "}
+                      </p>
+                    </a>
+                    {/* mint.owner が currentAccount なら edit ボタンを追加します。 */}
+                    {mint.owner.toLowerCase() ===
+                    currentAccount.toLowerCase() ? (
+                      <button
+                        className="edit-button"
+                        onClick={() => editRecord(mint.name)}
+                      >
+                        <img
+                          className="edit-icon"
+                          src="https://img.icons8.com/metro/26/000000/pencil.png"
+                          alt="Edit button"
+                        />
+                      </button>
+                    ) : null}
+                  </div>
+                  <p> {mint.record} </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    }
+  };
+
+  // edit モードを設定します。
+  const editRecord = (name) => {
+    console.log("Editing record for", name);
+    setEditing(true);
+    setDomain(name);
   };
 
   useEffect(() => {
@@ -266,8 +428,8 @@ const App = () => {
         </div>
 
         {!currentAccount && renderNotConnectedContainer()}
-        {/* アカウントが接続されるとインプットフォームをレンダリングします。 */}
         {currentAccount && renderInputForm()}
+        {mints && renderMints()}
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
