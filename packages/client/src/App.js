08b86da25b0d72ac0445f78f7e0c23221029bfbc
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-23 11:18:14 +0900

    feat: ユーザーのウォレットにアクセス可能か確認するまで

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,37 +1,49 @@
-import React, { useEffect } from "react";
+// useStateを追加でインポートしています。
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
 import catGif from "./assets/cat.gif";
 
-// 定数
 const TWITTER_HANDLE = "UNCHAIN_tech";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  // ウォレットの接続を確認します。
-  const checkIfWalletIsConnected = () => {
-    // window.ethereumの設定。この表記法はJavascriptの「分割代入」を参照。
+  //ユーザーのウォレットアドレスをstate管理しています。冒頭のuseStateのインポートを忘れないでください。
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
 
     if (!ethereum) {
-      console.log("Make sure you have MetaMask!");
+      console.log("Make sure you have metamask!");
       return;
     } else {
       console.log("We have the ethereum object", ethereum);
     }
+
+    // ユーザーのウォレットをリクエストします。
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    // ユーザーが複数のアカウントを持っている場合もあります。ここでは最初のアドレスを使います。
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
   };
 
-  // まだウォレットに接続されていない場合のレンダリングです。
+  // コネクトしていないときのレンダリング関数です。
   const renderNotConnectedContainer = () => (
     <div className="connect-wallet-container">
-      <img src={catGif} alt="Ninja gif" />
+      <img src={catGif} alt="Cat gif" />
       <button className="cta-button connect-wallet-button">
         Connect Wallet
       </button>
     </div>
   );
 
-  // ページがリロードされると呼び出されます。
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
@@ -48,7 +60,6 @@ const App = () => {
           </header>
         </div>
 
-        {/* render 関数をここに追加します */}
         {renderNotConnectedContainer()}
 
         <div className="footer-container">
@@ -58,9 +69,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >
-            {`built with @${TWITTER_HANDLE}`}
-          </a>
+          >{`built with @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
