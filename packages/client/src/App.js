3bc9e845fd445a4bb974c850e7717b46ed2f0677
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-23 11:41:04 +0900

    feat: S2-L2まで

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import { ethers } from "ethers";
 import catGif from "./assets/cat.gif";
+import contractAbi from "./utils/contractABI.json";
 
-// 定数
 const TWITTER_HANDLE = "UNCHAIN_tech";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+// 登録したいドメインです。好みで変えてみましょう。
+const tld = ".nyanko";
+const CONTRACT_ADDRESS = "0x1AB36905C54eBf7c5b7Be84832E85d37c00C2C77";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  // state管理するプロパティを追加しています。
+  const [domain, setDomain] = useState("");
+  const [record, setRecord] = useState("");
 
-  // connectWallet 関数を定義
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -20,12 +26,10 @@ const App = () => {
         return;
       }
 
-      // アカウントへのアクセスを要求するメソッドを使用します。
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
       });
 
-      // Metamask を一度認証すれば Connected とコンソールに表示されます。
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (error) {
@@ -54,11 +58,69 @@ const App = () => {
     }
   };
 
-  // レンダリング関数です。まだ Connect されていない場合。
+  const mintDomain = async () => {
+    // ドメインがnullのときrunしません。
+    if (!domain) {
+      return;
+    }
+    // ドメインが3文字に満たない、短すぎる場合にアラートを出します。
+    if (domain.length < 3) {
+      alert("Domain must be at least 3 characters long");
+      return;
+    }
+    // ドメインの文字数に応じて価格を計算します。
+    // 3 chars = 0.05 MATIC, 4 chars = 0.03 MATIC, 5 or more = 0.01 MATIC
+    const price =
+      domain.length === 3 ? "0.05" : domain.length === 4 ? "0.03" : "0.01";
+    console.log("Minting domain", domain, "with price", price);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractAbi.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas...");
+        let tx = await contract.register(domain, {
+          value: ethers.utils.parseEther(price),
+        });
+        // ミントされるまでトランザクションを待ちます。
+        const receipt = await tx.wait();
+
+        // トランザクションが問題なく実行されたか確認します。
+        if (receipt.status === 1) {
+          console.log(
+            "Domain minted! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          // domain,recordをセットします。
+          tx = await contract.setRecord(domain, record);
+          await tx.wait();
+
+          console.log(
+            "Record set! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          setRecord("");
+          setDomain("");
+        } else {
+          alert("Transaction failed! Please try again");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // レンダリング関数です。
   const renderNotConnectedContainer = () => (
     <div className="connect-wallet-container">
       <img src={catGif} alt="Cat gif" />
-      {/* Connect Wallet ボタンが押されたときのみ connectWallet関数 を呼び出します。 */}
+      {/* ボタンクリックでconnectWallet関数を呼び出します。 */}
       <button
         onClick={connectWallet}
         className="cta-button connect-wallet-button"
@@ -67,6 +129,37 @@ const App = () => {
       </button>
     </div>
   );
+
+  // ドメインネームとデータの入力フォームです。
+  const renderInputForm = () => {
+    return (
+      <div className="form-container">
+        <div className="first-row">
+          <input
+            type="text"
+            value={domain}
+            placeholder="domain"
+            onChange={(e) => setDomain(e.target.value)}
+          />
+          <p className="tld"> {tld} </p>
+        </div>
+
+        <input
+          type="text"
+          value={record}
+          placeholder="whats ur nyanko power?"
+          onChange={(e) => setRecord(e.target.value)}
+        />
+
+        <div className="button-container">
+          {/* ボタンクリックで mintDomain関数 を呼び出します。 */}
+          <button className="cta-button mint-button" onClick={mintDomain}>
+            Mint
+          </button>
+        </div>
+      </div>
+    );
+  };
 
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -84,8 +177,9 @@ const App = () => {
           </header>
         </div>
 
-        {/* currentAccount が存在しない場合、Connect Wallet ボタンを表示します*/}
         {!currentAccount && renderNotConnectedContainer()}
+        {/* アカウントが接続されるとインプットフォームをレンダリングします。 */}
+        {currentAccount && renderInputForm()}
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
