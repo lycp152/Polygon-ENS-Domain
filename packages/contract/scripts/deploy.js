c18be722bae2586b9339c682cb5a2534510ff459
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-23 10:46:38 +0900

    feat: S1-L5まで

--- packages/contract/scripts/deploy.js
+++ packages/contract/scripts/deploy.js
@@ -1,32 +1,36 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const main = async () => {
+  const domainContractFactory = await hre.ethers.getContractFactory("Domains");
+  const domainContract = await domainContractFactory.deploy("nyanko");
+  await domainContract.deployed();
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  console.log("Contract deployed to:", domainContract.address);
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  // domainをオリジナルにしましょう！
+  let txn = await domainContract.register("banana", {
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await txn.wait();
+  console.log("Minted domain banana.nyanko");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  txn = await domainContract.setRecord("banana", "Am I a banana or a nyanko??");
+  await txn.wait();
+  console.log("Set record for banana.nyanko");
 
-  await lock.deployed();
+  const address = await domainContract.signer.getAddress("banana");
+  console.log("Owner of domain banana:", address);
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+};
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
